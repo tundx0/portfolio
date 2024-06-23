08f420d9c76c10c88d5e48652d77b36d37dc4c84
Olatunde Adegboyebo <tundx0@gmail.com>
2024-06-23 23:31:27 +0100

    setup project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "profile-animate": "profile__animate 8s ease-in-out infinite",
+      },
+      keyframes: {
+        profile__animate: {
+          "0%": { borderRadius: "60% 40% 30% 70% / 60% 30% 70% 40%" },
+          "50%": { borderRadius: "30% 60% 70% 40% / 50% 60% 30% 60%" },
+          "100%": { borderRadius: "60% 40% 30% 70% / 60% 30% 70% 40%" },
+        },
+      },
     },
   },
   plugins: [],
