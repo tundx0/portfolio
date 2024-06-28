fbf64dec0695156a62e44522106f1621fc8bb73a
Olatunde Adegboyebo <tundx0@gmail.com>
2024-06-28 03:42:01 +0100

    WIP: add responsiveness on mobile devices

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 const Footer = () => {
   return (
-    <section id="about" className="pt-48 lg:mx-6 mx-4 gap-4 items-center">
+    <section
+      id="footer"
+      className="pt-24 md:pt-44 lg:mx-6 mx-4 gap-4 items-center flex flex-col"
+    >
       <h1 className="text-5xl text-center font-extrabold">Footer</h1>
     </section>
   );
--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,6 +1,9 @@
 const Portfolio = () => {
   return (
-    <section id="portfolio" className="pt-48 lg:mx-6 mx-4 gap-4 items-center">
+    <section
+      id="portfolio"
+      className="pt-24 md:pt-44 lg:mx-6 mx-4 gap-4 items-center flex flex-col"
+    >
       <h1 className="text-5xl text-center font-extrabold">Portfolio</h1>
     </section>
   );
