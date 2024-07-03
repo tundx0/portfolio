3df00f33c7c7c8031a03d748d0634c5021680591
Olatunde Adegboyebo <tundx0@gmail.com>
2024-07-03 23:59:56 +0100

    portfolio setup

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -5,6 +5,7 @@ const Portfolio = () => {
       className="pt-24 md:pt-44 lg:mx-6 mx-4 gap-4 items-center flex flex-col"
     >
       <h1 className="text-5xl text-center font-extrabold">Portfolio</h1>
+      <div className="md:pt-32 pt-16 grid grid-cols-2 gap-12"></div>
     </section>
   );
 };
