528385cab8d35ac725efcfd1bf4eaa390413ea09
Olatunde Adegboyebo <tundx0@gmail.com>
2024-08-01 10:13:48 +0100

    add next config

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "files.fm",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
