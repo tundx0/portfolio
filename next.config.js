02cc40837f47f5585d86cd7fcade161d98991f94
Olatunde Adegboyebo <tundx0@gmail.com>
2024-08-01 10:58:30 +0100

    edit config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "files.fm",
-        pathname: "**",
-      },
-    ],
-  },
+export const images = {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "media.licdn.com",
+      pathname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "files.fm",
+      pathname: "**",
+    },
+  ],
 };
 
 
